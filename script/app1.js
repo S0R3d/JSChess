08aacdcd3b0d1c20315fb822fc0e9e5d279c32b5
S0R3d <tommasoguidi55@gmail.com>
2021-05-11 18:19:56 +0200

    Update board

--- script/app1.js
+++ script/app1.js
@@ -1,8 +1,32 @@
+/* GLOBAL var */
 const DIM = 8
-const $board = $('.container')[0]
+const $board = $('#board')[0]
 
-const loadBoard = (board) => {
+const bPieces = [
+    '/img/b-tower.svg',
+    '/img/b-knight.svg',
+    '/img/b-bishop.svg',
+    '/img/b-queen.svg',
+    '/img/b-king.svg',
+    '/img/b-bishop.svg',
+    '/img/b-knight.svg',
+    '/img/b-tower.svg',
+    '/img/b-pawn.svg',
+]
 
+const wPieces = [
+    '/img/w-tower.svg',
+    '/img/w-knight.svg',
+    '/img/w-bishop.svg',
+    '/img/w-king.svg',
+    '/img/w-queen.svg',
+    '/img/w-bishop.svg',
+    '/img/w-knight.svg',
+    '/img/w-tower.svg',
+    '/img/w-pawn.svg',
+]
+
+function loadBoard() {
     for (let i = DIM; i > 0; i--) {
         let row = document.createElement('div')
         row.className = 'row ' + i
@@ -11,17 +35,17 @@ const loadBoard = (board) => {
             let col = document.createElement('div')
             col.className = 'col-sm'
             col.style.backgroundColor = j%2 === 0 ? '#fff' : '#000'
-            let content = document.createElement('span')
-            content.innerHTML = 'X'
-            col.appendChild(content)
+            let img = document.createElement('img')
+            col.appendChild(img)
             row.appendChild(col)
         }
         $board.appendChild(row)
     }
+}
 
-
-    //--- si puo implementare durante il caricamento soprastante
-    // let rows = document.getElementsByClassName('row')
+function addCoordinate(params) {
+    // // --- si puo implementare durante il caricamento soprastante
+    // let rows = document.querySelectorAll('.row')
 
     // for (let i = 0; i < DIM; i++) {
     //     let row = rows[i]
@@ -41,7 +65,46 @@ const loadBoard = (board) => {
     // }
 }
 
+function loadPiece() {
+    let imgs = document.querySelectorAll('img')
+
+    for (let i = 0; i < DIM; i++) {
+        let bSrc = bPieces[i]
+        let wSrc = wPieces[i]
+
+        let img = imgs[i]
+        img.src = bSrc
+        img.style = 'padding-top: 8px;'
+
+        let img1 = imgs[i+56]
+        img1.src = wSrc
+        img1.style = 'padding-top: 8px;'
+    }
+
+    for (let i = 0; i < DIM; i++) {
+        let bSrc = bPieces[8]
+        let wSrc = wPieces[8]
+
+        imgs[i+8].src = bSrc
+        imgs[i+56-8].src = wSrc
+    }
+
+}
+
+function move() {
+    console.log('move');
+}
+
+function debug() {
+    console.log(document.querySelectorAll('.row'))
+    console.log(document.querySelectorAll('.col-sm'))
+    console.log(document.querySelectorAll('img'))
+}
 $(() => {
-    loadBoard($board)
-    console.log($board)
+    loadBoard()
+    loadPiece()
+    move()
+
+
+    debug()
 })
